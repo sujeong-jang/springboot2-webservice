131d08bc8759b2782edb47336a072aba779714b1
Sujeong Jang <56670564+sujeong-jang@users.noreply.github.com>
2020-07-06 15:25:56 +0900

    등록/수정/조회 API 만들기

---
+++ src/main/java/com/suzung/springboot/web/PostApiController.java
@@ -0,0 +1,35 @@
+package com.suzung.springboot.web;
+
+import com.suzung.springboot.service.posts.PostsService;
+import com.suzung.springboot.web.dto.PostsResponseDto;
+import com.suzung.springboot.web.dto.PostsSaveRequestDto;
+import com.suzung.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostApiController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+}
+
+
+
+
